9a2155ed0551c7d86237fb528ede65c581300ea8
visiilhaq <visiilhaq15@gmail.com>
2023-04-30 22:43:25 +0700

    menambahkan data.php untuk menampilkan total data

--- php/data.php
+++ php/data.php
@@ -10,7 +10,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc(); 
-    echo "Total data = " . $row["Data"];
+    echo "Total = " . $row["Data"];
 } else {
     echo "0 results";
 }
