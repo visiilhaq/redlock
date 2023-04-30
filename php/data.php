27321f218e3ec3b9a8fedf8bb38a42001098f94a
visiilhaq <visiilhaq15@gmail.com>
2023-04-30 22:32:19 +0700

    menambahkan data.php untuk menampilkan total data

---
+++ php/data.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn = mysqli_connect("172.18.0.3", "root", "utsSNA_php","Redlock");
+if (!$conn) {
+  die("Connection failed: " . mysql_connect_error());
+}
+$sql = "SELECT COUNT(*) as Data FROM users";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc(); 
+    echo "Total data = " . $row["Data"];
+} else {
+    echo "0 results";
+}
+?>
+
